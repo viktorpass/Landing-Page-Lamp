d58ef9bf56ae3c9b60e733ba2b73f91a8acedeb9
viktorpass <vkfurieri21@gmail.com>
2021-10-26 18:38:51 -0300

    Projeto Lamp

---
+++ turnOn.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("btn");
+let light = document.getElementById("light");
+        
+function toggleButton(){
+    button.classList.toggle("active");
+    light.classList.toggle("on");
+}
